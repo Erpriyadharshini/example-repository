6dc29f010976cae92c1efa0a6cbc25d19c01871a
Priyadharshini <priyadharshini24@gmail.com>
2025-08-30 07:06:50 -0700

    list2min_max

---
+++ list2min_max.py
@@ -0,0 +1,8 @@
+'''find minimum and maximum value in the list'''
+list=[25,5,15,77,123,9]
+def min_max(number):
+    maximumvalue=max(list)
+    minimumvalue=min(list)
+    print('maximum:',maximumvalue)
+    print("minimum:",minimumvalue)
+min_max(list)
